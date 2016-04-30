2ccac1897e525de7b186685b79acf16c4892134d
mohitjss4git <kanpur.mohit@gmail.com>
2016-05-01 00:40:48 +0100

    Firdt Commit

---
+++ HelloWorld/src/com/java/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.sample;
+
+/**
+ * Created by Mohit_Saxena03 on 5/1/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
